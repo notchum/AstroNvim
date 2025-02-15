8390ff36f367a92cbbf8e7789a6ae3a932d0dad8
notchum <45953494+notchum@users.noreply.github.com>
2025-02-15 09:41:58 -0700

    Update user.lua

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
 
@@ -14,6 +12,11 @@ return {
     event = "BufRead",
     config = function() require("lsp_signature").setup() end,
   },
+
+  -- {
+  --   "notchum/black.nvim",
+  --   config = function() vim.cmd [[colorscheme black]] end,
+  -- },
 
   -- == Examples of Overriding Plugins ==
 
