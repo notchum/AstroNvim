fd55f1fb17343c9a12cbe0dfbb6e9eed5b70ed84
notchum <45953494+notchum@users.noreply.github.com>
2025-02-15 09:43:35 -0700

    Update community.lua

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,6 +5,20 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.lackluster-nvim" },
   -- import/override with your plugins folder
+  -- bars and lines
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
+  { import = "astrocommunity.bars-and-lines.lualine-nvim" },
+  -- diagnostics
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  -- language packs
+  -- { import = "astrocommunity.pack.bash" }, TODO: fix this (npm not executable in Mason)
+  { import = "astrocommunity.pack.cpp" },
+  -- { import = "astrocommunity.pack.json" }, TODO: fix this (npm not executable in Mason)
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.rust" },
 }
